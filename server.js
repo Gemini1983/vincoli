3ec90c20746bca3f149f9db3cac86a67068c8518
Andrea Annunziata <a.annunziata@informatica.aci.it>
2017-07-04 13:42:46 +0200

    fix

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-//var app = require('./server/server.js')  
-//app.start(); 
+var app = require('./server/server.js')  
+app.start(); 
 
-
+/*
 var http = require('http');
 var util = require('util');
-var app_port = 8080;
-var app_host = 'localhost';
+var app_port = process.env.app_port || 8080;
+var app_host = process.env.app_host || 'localhost';
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -13,3 +13,4 @@ http.createServer(function(req, res) {
     res.end();
 }).listen(app_port);
 console.log('Web server running at http://' + app_host + ':' + app_port);
+*/
