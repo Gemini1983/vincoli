29654a2231953e8c8f8c4b6501e8087be2f7fcfe
Andrea Annunziata <a.annunziata@informatica.aci.it>
2024-03-15 21:20:26 +0100

    Evoluzione express e node

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
-var app = require('./server/server.js')  
-app.start(); 
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-/*
-var http = require('http');
-var util = require('util');
-var app_port = process.env.app_port || 8080;
-var app_host = process.env.app_host || 'localhost';
+const app = express();
+const port = 3000; // Puoi usare qualsiasi porta disponibile
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World from Cloudnode\n\n');
-    res.end();
-}).listen(app_port);
-console.log('Web server running at http://' + app_host + ':' + app_port);
-*/
+
+const uri = "mongodb://webusr:wmLf6Uu00t3WRuAT@vincoli-shard-00-00.upvff.mongodb.net:27017,vincoli-shard-00-01.upvff.mongodb.net:27017,vincoli-shard-00-02.upvff.mongodb.net:27017/vincoli?ssl=true&replicaSet=atlas-vmzue4-shard-0&authSource=admin&retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.static('client')); // Aggiungi questa riga
+
+
+app.get('/api', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db("vincoli"); // Nome del tuo database
+        const collection = database.collection('vincoli'); // Nome della tua collezione
+
+        // Puoi personalizzare la query in base alle tue necessità
+        const query = { foglio: req.query.foglio, particella: req.query.particella };
+        const data = await collection.findOne(query);
+
+        if (data) {
+            res.json(data);
+        } else {
+            res.status(404).send('Dati non trovati');
+        }
+    } catch (e) {
+        res.status(500).send('Errore del server');
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+});
